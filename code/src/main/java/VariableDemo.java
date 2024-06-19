d552aa7cb3b19f0941da2b6681b5c65eb9bdc611
cgx <xuchenggonglx@qq.com>
2024-06-19 22:41:04 +0800

    xcg：变量和常量; 只看了 P26(下次 先紧着这块) 学完之后再其他内容

---
+++ code/src/main/java/VariableDemo.java
@@ -0,0 +1,31 @@
+/**
+ * @ClassName: VariableDemo
+ * @desc:
+ * @version:
+ * @author: xcg
+ * @date: 2024/6/19 21:59
+ * @since: jdk8
+ */
+public class VariableDemo {
+    // 实例变量 不初始化值 会是默认值
+    byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    boolean flag;
+    char c;
+    public static void main(String[] args) {
+        VariableDemo variable = new VariableDemo();
+        System.out.println("byte = " + variable.b);
+        System.out.println("short = " + variable.s);
+        System.out.println("int = " + variable.i);
+        System.out.println("long = " + variable.l);
+        System.out.println("float = " + variable.f);
+        System.out.println("double = " + variable.d);
+        System.out.println("boolean = " + variable.flag);
+        System.out.println("char = " + variable.c);
+        System.out.println((int)variable.c);
+    }
+}
