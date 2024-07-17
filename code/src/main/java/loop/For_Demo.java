effd968bb51c74215aa3d9ca337f07ecaefd727b
cgx <xuchenggonglx@qq.com>
2024-07-17 22:13:57 +0800

    xcg：先这样吧, 循环结构。其实没看多少内容

---
+++ code/src/main/java/loop/For_Demo.java
@@ -0,0 +1,25 @@
+package loop;
+
+/**
+ * @ClassName: For_Demo
+ * @desc:
+ * @version:
+ * @author: xcg
+ * @date: 2024/7/17 21:35
+ * @since: jdk8
+ */
+public class For_Demo {
+    public static void main(String[] args) {
+//        1. 计算0到100之间的奇数 及 偶数的和
+
+
+//        死循环
+//        for (;;) {
+//            System.out.println("--");
+//        }
+
+        // 2. 用 while 或 for 循环输出 1-1000 之间能被5 整除的数，并且每行输出 3 个
+
+        // 3. 打印九九乘法表
+    }
+}
