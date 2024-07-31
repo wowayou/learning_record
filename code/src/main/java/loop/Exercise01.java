968dfdc2122171c80efb4fdc11770041b8ead2cd
cgx <xuchenggonglx@qq.com>
2024-07-31 22:05:35 +0800

    Revert "xcg：脑子 先这样"
    This reverts commit 264ae19ec75e534789ad9c956d14c734d1bc0310.

--- code/src/main/java/loop/Exercise01.java
+++ code/src/main/java/loop/Exercise01.java
@@ -11,31 +11,19 @@ package loop;
 public class Exercise01 {
     public static void main(String[] args) {
         // 打印一个五行的三角形
-        // 最后一行要打印 2*5 - 1 9个※；第一行一个※ 其他都是两个※ [这是自己想的，实际是一个实心三角；但自己这个该怎么实现?]
+        // 最后一行要打印 2*5 - 1 9个※；第一行一个※ 其他都是两个※
         // 每一行都是 ※和空格组成的
-        // 只在特定的地方打印 ※，其他地方输出空格
-        // 分部 对称。先具体 再抽象 逐渐完成
-
-        // 思路还是没厘清，再想一下
+        // 只在特定的地方打印 ※，其他地方输出空格 > 明天再说
         int n = 5; // 要打印的行数
-        for (int i = 1; i <= n; i++) {
-            for(int j = 5; j > i; j--) {
-                System.out.print(" ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= 2 * n - 1; j++) {
+                if (j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
-            System.out.print("*");
-            for(int j = 1; j < i; j++) {
-                System.out.print("*");
-            }
-            System.out.print(" ");
-            for(int j = 1; j < i; j++) {
-                System.out.print(" ");
-            }
-            System.out.print("*");
-            for(int j = 5; j > i; j--) {
-                System.out.print("*");
-            }
-            System.out.println(" ");
-
+            System.out.println();
         }
     }
 }
