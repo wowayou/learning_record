4a45efaff6d03d9c0d969dcd786f1c7ec88d42bd
cgx <xuchenggonglx@qq.com>
2024-10-16 21:17:16 +0800

    xcg: 值传递和引用传递 2024年10月16日 再次开始, 可以坚持的

---
+++ code/src/main/java/oop/ParameterPassing.java
@@ -0,0 +1,37 @@
+package oop;
+
+/**
+ * @ClassName: ParameterPassing
+ * @desc: 函数的传参方式; Java中实质上是值传递（传递的是参数的副本）, 之所以会在传递对象
+ * 数组的时候表现出引用传递（传递的是地址）的特征, 是因为传递的是引用的副本。也是指向实际位置的
+ * @version:
+ * @author: xcg
+ * @date: 2024/10/16 20:49
+ * @since: jdk8
+ */
+public class ParameterPassing {
+    public static void main(String[] args) {
+        int i = 10;
+        valueTrans(i);
+        System.out.println("i = " + i);
+
+        Animal animal = new Animal();
+        animal.age = 10;
+        referenceTrans(animal);
+        System.out.println("animal.age = " + animal.age);
+
+    }
+
+    private static void referenceTrans(Animal animal) {
+        animal.age=100;
+        animal.gender="male";
+    }
+
+    private static void valueTrans(int i) {
+        i = 20;
+    }
+}
+class Animal {
+    int age;
+    String gender;
+}
